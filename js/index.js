6a23dd49084569bf6c26d84d90c9fa574dbbf1ae
raziel <ou66ou88@hotmail.com>
2018-02-02 10:53:14 +0800

    页面数据填充

--- js/index.js
+++ js/index.js
@@ -3,35 +3,49 @@ var vm = new Vue({
     data:{
         binner:[],
         currentIndex: 0,
-        timer: ''
+        timer: '',
+        transition:"list"
     },
     methods:{
-        go:function() {
+        go:function() {//定时轮播
             this.timer = setInterval(() => {
                 this.autoPlay()
             }, 4000)
         },
-        stop:function() {
+        stop:function() {//暂停播放
             clearInterval(this.timer)
             this.timer = null
         },
-        change:function(index) {
-            this.currentIndex = index
-        },
-        autoPlay:function() {
+        autoPlay:function() {//下一个
+            this.transition="list";
             this.currentIndex++
             if (this.currentIndex > this.binner.length - 1) {
                 this.currentIndex = 0
             }
+        },
+        left:function(){//向左滑动
+            this.stop();
+            this.autoPlay();
+            this.go();
+        },
+        right:function(){//向右滑动
+            this.transition="list2";
+            this.stop();
+            this.currentIndex--;
+            if (this.currentIndex < 0) {
+                this.currentIndex = this.binner.length - 1
+            }
+            this.go();  
         }
     },
     created:function(){
-        axios.get("http://192.168.1.107:8080/oneqrcode/shopBannerController/query.do").then(function(response){
+        show_loading();//显示加载动画
+        axios.get("http://192.168.1.107:8080/oneqrcode/shopBannerController/query.do").then(function(response){//请求轮播图数据
             console.log(response);
             vm.binner = response.data.data;
-            
+            hide_loading();//加载完成
         })
-        this.$nextTick(function(){
+        this.$nextTick(function(){//启动轮播图
             vm.timer = setInterval(function(){
                 vm.autoPlay()
             }, 4000)

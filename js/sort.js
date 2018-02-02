e2e410d4a9215dd874ed1359ed45c9d68b4cad6a
raziel <ou66ou88@hotmail.com>
2018-02-02 18:13:22 +0800

    分类页效果修改

--- js/sort.js
+++ js/sort.js
@@ -1,15 +1,32 @@
 var vm = new Vue({
     el:".container",
     data:{
-        search:""
+        search:"",
+        arr:[],
+        sortIndex:0
     },
     methods:{
         search_shop:function(){
             sessionStorage.search_id=this.search;
             window.location.href="./searchshop.html"
+        },
+        toggle:function(index){
+            if(this.sortIndex==index){
+                this.sortIndex=-1;
+            }else{
+                this.sortIndex=index;
+            }
+            
         }
     },
     created:function(){
+        show_loading();
+        axios.get("http://192.168.1.107:8080/oneqrcode/firstSortController/query.do").then(function(response){
+            console.log(response.data.data);
+            vm.arr = response.data.data;
+            hide_loading();
+        }).catch(function(error){
 
+        })
     }
 })

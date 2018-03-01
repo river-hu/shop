c32e8beed5d228387eecd5b3db62a5a203fdc817
raziel <ou66ou88@hotmail.com>
2018-03-01 17:02:52 +0800

    修改我的订单页

--- shop/js/mybuy.js
+++ shop/js/mybuy.js
@@ -1,14 +1,18 @@
+Vue.filter('date', function (value) {
+    if (!value) return '';
+          return value.substring(5).substr(0,11);
+  })
 var vm=new Vue({
     el:".container",
     data:{
-        off:false,
-        off_text:"点击查看物流信息",
         nin:5,
         num:'',
         num_off:false,
         arrindex:0,
         userid:0,
         items:[],
+        shop:[],
+        itemindex:-1,
         arrnav:[
             {
                 name:'全部订单',
@@ -32,7 +36,11 @@ var vm=new Vue({
             '未发货',
             '待评价',
             '已评价'
-        ]
+        ],
+        address:[],
+        off:false,
+        shopindex:0
+
     },
     watch:{
         num:function(){
@@ -46,43 +54,101 @@ var vm=new Vue({
         }
     },
     methods:{
-        wuliu:function(){
-            this.off=!this.off;
-            this.off_text=this.off?"点击收起":"点击查看物流信息";
-        },
-        confirm:function(){
-            if(confirm("是否确认收货")){
-
+        wuliu:function(index){
+            this.itemindex=this.itemindex==index?-1:index;   
+            if(this.itemindex!=-1){
+                show_loading();
+                axios.get("http://yunzhujia.qx1688.net/oneqrcode/shopOrderController/getExpressInfo.do",{
+                    params:{
+                        id:vm.items[vm.itemindex].id
+                    }
+                }).then(function(response){
+                    vm.address = response.data.data.map.Traces.myArrayList;
+                    hide_loading();
+                }).catch(function(error){
+                })
             }
+        },
+        submit:function(){
+            show_loading();
+            axios.get("http://yunzhujia.qx1688.net/oneqrcode/shopGoodsEvaluationController/add.do",{
+                params:{
+                    'wechatUserId':vm.userid,
+                    'goodsId':vm.items[vm.shopindex].goodsId,
+                    'orderId':vm.items[vm.shopindex].id,
+                    'content':vm.num,
+                    'star':vm.nin
+                }
+            }).then(function(response){
+                console.log(response.data);
+                vm.off = false;
+                var userid = vm.userid;
+                axios.get("http://yunzhujia.qx1688.net/oneqrcode/shopOrderController/query.do",{
+                    params:{
+                        'wechat_user_id':userid,
+                        'page':1,
+                        'count':1000
+                    }
+                }).then(function(response){
+                    console.log(response.data);
+                    vm.shop = response.data.data.list;
+                    vm.toggle(vm.arrindex);
+                    hide_loading();
+                }).catch(function(error){
+                })
+            }).catch(function(error){
+            })
+        },
+        pp:function(index){//显示商品评价面板
+                this.off = true;
+                this.shopindex = index;
         },
         evaluate:function(index){
-           
             if(typeof index =='number'){
                 
-           this.nin=index;
+                 this.nin=index;
             }
+        },
+        del:function(){
+            this.num = '';
+            this.nin = 5;
+            this.off = false;
         },
         toggle:function(index){
             this.arrindex = index;
+            this.items = [];
+            this.itemindex = -1;
+            if(index!=0){
+                for(var i in this.shop){
+                    if(this.shop[i].state==index){
+                        this.items.push(this.shop[i]);
+                    }
+                }
+            }else{
+                this.items = this.shop;
+            }
         }
     },
     created:function(){
-        // this.userid = sessionStorage.userid;
-        // if(this.userid==''||this.userid==undefined||this.userid=='undefined'){
-        //     window.location.href="../user/index.html";
-        // }
-        // var userid = this.userid;
+        this.userid = sessionStorage.userid;
+        if(this.userid==''||this.userid==undefined||this.userid=='undefined'){
+            window.location.href="../user/index.html";
+        }
+        var userid = this.userid;
+        show_loading();
         this.userid = 33;
         var userid = this.userid;
         axios.get("http://yunzhujia.qx1688.net/oneqrcode/shopOrderController/query.do",{
             params:{
                 'wechat_user_id':userid,
                 'page':1,
-                'count':10
+                'count':1000
             }
         }).then(function(response){
             console.log(response.data);
-            vm.items = response.data.data.list;
+            vm.shop = response.data.data.list;
+            vm.items = vm.shop;
+            hide_loading();
         }).catch(function(error){
         })
     }
